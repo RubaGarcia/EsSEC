f1077d9ae2bfacb6069a2fd9c86d94a0a3a6b5f3
DESKTOP-UKC22T0\Incentro <ruben.garcia@incentro.com>
2024-08-07 09:38:20 +0200

    Añadidas Rutas y nueva forma de arrancar la API(para que no pille errores de CORS)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,16 @@
 import express from 'express';
+import { corsConfig } from './config/cors';
+import cors from 'cors';
+import morgan from 'morgan';
+
 
 const app = express();
+
+app.use(cors(corsConfig))
+app.use(morgan('dev'))
+
+app.use(express.json())
+
+//routes
 
 export default app
