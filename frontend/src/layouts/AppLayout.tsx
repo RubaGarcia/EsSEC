b77ee2863d2787f7763a55bc9757924dfba1d16e
Eduardo Llamosas <eduardo.llamosas@incentro.com>
2024-08-20 10:44:45 +0200

    Moved types folder to front logic

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -4,60 +4,36 @@ import Header from "../components/layout/Header";
 import { getElements } from "../api/LayoutAPI";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
-type ApiResponse = {
-  metadata: object;
-  sys: {
-    space: object;
-    id: string;
-    type: string;
-    createdAt: string;
-    updatedAt: string;
-    environment: object;
-    revision: number;
-    contentType: object;
-    locale: string;
-  };
-  fields: {
-    pageTitle: string;
-    internalTitle: string;
-    slug: string;
-    seoMetadata: object;
-    header: object;
-    sections: object[];
-    footer: object;
-  };
-}
-
 
 
 
 export default function AppLayout() {
 
 
-  // let localHeader= sessionStorage.getItem('Header');
-  // let localFooter= sessionStorage.getItem('Footer');
-  // console.log(localHeader)
-  // console.log(localFooter)
+  let localHeader= sessionStorage.getItem('Header');
+  let localFooter= sessionStorage.getItem('Footer');
+  console.log(localHeader)
+  console.log(localFooter)
 
-  // if( localHeader === null || localFooter === null){
-  //   const { data, error, isLoading } = useQuery({
-  //     queryKey: ["elements"],
-  //     queryFn: getElements,
-  //   })
-  //   const response: ApiResponse = data;
+  if( localHeader === null || localFooter === null){
+    const { data, error, isLoading } = useQuery({
+      queryKey: ["elements"],
+      queryFn: getElements,
+    })
+    const response = data;
     
   
-  //   const header= response.fields.header;
-  //   const footer= response.fields.footer;
+    const header= response.fields.header;
+    const footer= response.fields.footer;
 
-  //   sessionStorage.setItem('Header', JSON.stringify(header));
-  //   sessionStorage.setItem('Footer', JSON.stringify(footer));
-  //   console.log(data);
-  //   if (isLoading) return <p>Loading...</p>
-  // } else{
-  //   const header=JSON.parse(localHeader);
-  //   const footer=JSON.parse(localFooter);
-  // }
+    sessionStorage.setItem('Header', JSON.stringify(header));
+    sessionStorage.setItem('Footer', JSON.stringify(footer));
+    console.log(data);
+    if (isLoading) return <p>Loading...</p>
+  } else{
+    const header=JSON.parse(localHeader);
+    const footer=JSON.parse(localFooter);
+  }
 
 
   
