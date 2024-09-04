edea2a5e0b5485b064169519ac7cf7575000c2e9
DESKTOP-UKC22T0\Incentro <ruben.garcia@incentro.com>
2024-09-04 12:55:19 +0200

    TICKET: fixed reviews

--- frontend/src/views/MainView.tsx
+++ frontend/src/views/MainView.tsx
@@ -2,72 +2,72 @@ import { useQuery } from "@tanstack/react-query";
 import PortfolioDisplay from "../components/PortfolioDisplay";
 import ReviewSlider from "../components/ReviewSlider";
 import { getPage } from "../api/HomeAPI";
-import {PersonFields, ValuePropositionFields, Cartridge } from "../types";
+import { PersonFields, ValuePropositionFields, Cartridge } from "../types";
 import type { ApiRequest, Entry, Blurb } from "../types";
 
 export default function MainView() {
-  const { data/* ,error */, isLoading } :  {data: undefined | ApiRequest, error: null | Error, isLoading: boolean} = useQuery({
-    queryKey: ["HomePage"],
-    queryFn: getPage,
-  });
+  const {
+    data /* ,error */,
+    isLoading,
+  }: { data: undefined | ApiRequest; error: null | Error; isLoading: boolean } =
+    useQuery({
+      queryKey: ["HomePage"],
+      queryFn: getPage,
+    });
 
-  if (isLoading) return <p>Loading...</p>
+  if (isLoading) return <p>Loading...</p>;
 
   const elements = data?.fields?.sections;
- 
-    const characteristics = elements?.find(
-      (element : Entry<Cartridge> | Entry<Blurb> ) => element.fields?.internalTitle === "characteristics",
-    );
-  
 
-      const aux = characteristics as Entry<Blurb>
+  const characteristics = elements?.find(
+    (element: Entry<Cartridge> | Entry<Blurb>) =>
+      element.fields?.internalTitle === "characteristics",
+  );
 
+  const aux = characteristics as Entry<Blurb>;
 
-  const heroCartridge: Entry<Cartridge>= data?.fields?.sections![0] as Entry<Cartridge>
-  const heroValueProp: Entry<ValuePropositionFields>= heroCartridge?.fields?.items![0] as  Entry<ValuePropositionFields>;
-  const texts :string[]= aux.fields?.list!
+  const heroCartridge: Entry<Cartridge> = data?.fields
+    ?.sections![0] as Entry<Cartridge>;
+  const heroValueProp: Entry<ValuePropositionFields> = heroCartridge?.fields
+    ?.items![0] as Entry<ValuePropositionFields>;
+  const texts: string[] = aux.fields?.list!;
 
-  const valuePropList: Entry<Cartridge>= data?.fields?.sections![1] as Entry<Cartridge>
- 
-  
+  const valuePropList: Entry<Cartridge> = data?.fields
+    ?.sections![1] as Entry<Cartridge>;
+
   function PortfolioFields(): string[] {
     const types: string[] = [];
 
+    const items: Entry<ValuePropositionFields>[] = valuePropList.fields
+      ?.items as Entry<ValuePropositionFields>[];
 
-      const items : Entry<ValuePropositionFields>[] = valuePropList.fields?.items as Entry<ValuePropositionFields>[];
-      
-      // Recorre cada elemento en items y extrae el campo 'type'
-      for (const item of items) {
-        const type = item.fields?.type;
-        if (type) {
-          types.push(type);
-        }
+    // Recorre cada elemento en items y extrae el campo 'type'
+    for (const item of items) {
+      const type = item.fields?.type;
+      if (type) {
+        types.push(type);
       }
-  
+    }
+
     return types;
   }
 
-  
+  const personList: Entry<Cartridge> = data?.fields
+    ?.sections?.[2] as Entry<Cartridge>;
 
-  const personList: Entry<Cartridge>= data?.fields?.sections?.[2]  as Entry<Cartridge>;
+  const reviewer: Entry<PersonFields> = personList.fields
+    ?.items?.[0] as Entry<PersonFields>;
 
-  const reviewer: Entry<PersonFields> = personList.fields?.items?.[0] as Entry<PersonFields>;
+  let listReview: Entry<PersonFields>[] = personList.fields
+    ?.items as Entry<PersonFields>[];
 
+  const heroImgUrl =
+    heroValueProp.fields?.icon?.fields?.asset?.fields?.file?.url;
 
-  let listReview: Entry <PersonFields>[] = personList.fields?.items as Entry<PersonFields>[];
-  
-  
-  const heroImgUrl= heroValueProp.fields?.icon?.fields?.asset?.fields?.file?.url;
-
-
-
-  const portFolioElements  : Entry<ValuePropositionFields>[] = valuePropList.fields?.items as Entry<ValuePropositionFields>[] ?? [];
-
+  const portFolioElements: Entry<ValuePropositionFields>[] =
+    (valuePropList.fields?.items as Entry<ValuePropositionFields>[]) ?? [];
 
   reviewer && listReview?.push(reviewer);
-
-
-
 
   return (
     <>
@@ -100,10 +100,10 @@ export default function MainView() {
               </div>
             ))}
 
-            <div className="w-full mt-8 bg-transparent border rounded-md lg:max-w-sm dark:border-gray-700 focus-within:border-blue-400 focus-within:ring focus-within:ring-blue-300 dark:focus-within:border-blue-400 focus-within:ring-opacity-40">
-              <form 
-              className="flex flex-col lg:flex-row"
-              //TODO: onSubmit={}
+            {/* <div className="w-full mt-8 bg-transparent border rounded-md lg:max-w-sm dark:border-gray-700 focus-within:border-blue-400 focus-within:ring focus-within:ring-blue-300 dark:focus-within:border-blue-400 focus-within:ring-opacity-40">
+              <form
+                className="flex flex-col lg:flex-row"
+                //TODO: onSubmit={}
               >
                 <input
                   type="email"
@@ -117,30 +117,25 @@ export default function MainView() {
                   Contact Us
                 </button>
               </form>
-            </div>
+            </div> */}
           </div>
         </div>
 
         <div className="flex items-center justify-center w-full h-96 lg:w-1/2">
           <img
             className="object-cover w-full h-full mx-auto rounded-md lg:max-w-2xl"
-            src={heroImgUrl}//"https://images.unsplash.com/photo-1543269664-7eef42226a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            src={heroImgUrl} //"https://images.unsplash.com/photo-1543269664-7eef42226a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
             alt="glasses photo"
           />
         </div>
       </div>
-      
-      <PortfolioDisplay 
-        fields={PortfolioFields()} 
+
+      <PortfolioDisplay
+        fields={PortfolioFields()}
         elements={portFolioElements ? portFolioElements : []}
       />
 
-
-      <ReviewSlider 
-        reviewer={listReview}
-      />
+      <ReviewSlider reviewer={listReview} />
     </>
   );
 }
-
-
