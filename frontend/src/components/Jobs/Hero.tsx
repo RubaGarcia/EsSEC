a7952846f39c3e04b47369906675b925b22ec85c
DESKTOP-UKC22T0\Incentro <ruben.garcia@incentro.com>
2024-09-04 13:14:38 +0200

    fix:image on general jobs view

--- frontend/src/components/Jobs/Hero.tsx
+++ frontend/src/components/Jobs/Hero.tsx
@@ -52,8 +52,7 @@ export default function Hero({ hero }: HeroProps) {
         <div
           className="w-full h-full bg-cover"
           style={{
-            backgroundImage: `url(https:${heroImgUrl})`
-            //"url(https://images.unsplash.com/photo-1508394522741-82ac9c15ba69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=748&q=80)",
+            backgroundImage: `url(https:${heroImgUrl})`,
           }}
         >
           <div className="w-full h-full bg-black opacity-25"></div>
