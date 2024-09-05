fdf2e6e7900df2bb2b9b869d928a17f6beb9fa7b
Eduardo Llamosas <eduardo.llamosas@incentro.com>
2024-09-05 15:06:31 +0200

    typo en jobitem

--- frontend/src/components/Jobs/JobItem.tsx
+++ frontend/src/components/Jobs/JobItem.tsx
@@ -13,7 +13,7 @@ export default function JobItem({ job }: JobItemProps) {
           {job.fields?.name}
         </h3>
         <p className="mt-2 text-sm text-gray-500 dark:text-gray-400 font-bold">
-          {job.fields?.salary?.toString()}€/mes
+          {job.fields?.salary?.toString()}€/año
         </p>
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-400">
