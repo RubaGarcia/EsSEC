b3a52a104b6da4f134bb36620d8544dc75fde5e4
Eduardo Llamosas <eduardo.llamosas@incentro.com>
2024-09-05 12:01:16 +0200

    minor changes to auditories

--- frontend/src/views/Services/AuditoryServiceView.tsx
+++ frontend/src/views/Services/AuditoryServiceView.tsx
@@ -42,7 +42,7 @@ export default function AuditoryServiceView() {
           <p className="text-xl font-medium text-blue-500 ">Testimonials</p>
 
           <h1 className="mt-2 text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
-            What clients saying
+            What the clients are saying...
           </h1>
 
           <TestimonialAuditories reviews={reviews}/>
