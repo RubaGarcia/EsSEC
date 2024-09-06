4e0654280cd410cdaa68093ccf0fa49a0aea28c1
DESKTOP-UKC22T0\Incentro <ruben.garcia@incentro.com>
2024-09-06 12:30:44 +0200

    disabling button of apply when not possible to apply and style of appliances button

--- frontend/src/views/Jobs/JobDetailView.tsx
+++ frontend/src/views/Jobs/JobDetailView.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {  useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { applyJob, getJobById } from "../../api/JobsAPI";
 import { useParams } from "react-router-dom";
 import { renderRichText } from "../../helpers/RichTextProcessor";
@@ -18,41 +18,54 @@ export default function JobDetailView() {
     setIsModalOpen(false);
   }
 
-  function handleFormSubmit(Userdata: { firstName: string; lastName: string; email: string; files: File[] }) {
+  function handleFormSubmit(Userdata: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    files: File[];
+  }) {
     console.log("Datos enviados:", Userdata);
-    
+
     const formData = new FormData();
     formData.append("email", Userdata.email);
     formData.append("firstName", Userdata.firstName);
     formData.append("lastName", Userdata.lastName);
 
-    
-    
     Userdata.files.forEach((file) => formData.append("files", file));
-    
+
     if (!data?.applicants) {
       alert("No se puede aplicar a esta oferta de trabajo");
       return;
     }
-    
+
     formData.append("applicantsList", data.applicants.sys?.id ?? "");
-    
+
     localStorage.setItem("formData", JSON.stringify(Userdata));
-    
-    toast("Datos enviados con éxito", { type: "success" })
-    applyJob({ formData, jobId: "job" }).then(() => {
-      console.log("Datos enviados con éxito");
-      handleModalClose();
-    }).catch((error) => {
-      console.error("Error al enviar los datos:", error);
-      alert("Ocurrió un error al enviar los datos");
-    });
+
+    toast("Datos enviados con éxito", { type: "success" });
+    applyJob({ formData, jobId: "job" })
+      .then(() => {
+        console.log("Datos enviados con éxito");
+        handleModalClose();
+      })
+      .catch((error) => {
+        console.error("Error al enviar los datos:", error);
+        alert("Ocurrió un error al enviar los datos");
+      });
   }
 
   const params = useParams();
   const jobId = params.jobId!;
-  
-  const { data, isError, isLoading } : {data: undefined | JobFields, isError: null | boolean, isLoading: boolean}= useQuery({
+
+  const {
+    data,
+    isError,
+    isLoading,
+  }: {
+    data: undefined | JobFields;
+    isError: null | boolean;
+    isLoading: boolean;
+  } = useQuery({
     queryKey: ["JobDetailPage", jobId],
     queryFn: () => getJobById({ JobId: jobId }),
     retry: 10,
@@ -65,14 +78,12 @@ export default function JobDetailView() {
   if (isError) {
     return <div>Ha ocurrido un error al cargar los datos.</div>;
   }
-  console.log()
-  console.log("applicants", data?.applicants)
-  console.log("employeeList", data?.employees)
-
+  console.log();
+  console.log("applicants", data?.applicants);
+  console.log("employeeList", data?.employees);
 
   return (
     <>
-    
       <div className="container px-6 py-16 mx-auto">
         <div className="items-center lg:flex">
           <div className="w-full lg:w-1/2">
@@ -81,7 +92,8 @@ export default function JobDetailView() {
                 Trabaja con <span className="text-blue-500">Nosotros</span>
               </h1>
               <p className="mt-3 text-gray-600 dark:text-gray-400">
-                Oferta de <span className="font-medium text-blue-500">trabajo</span>
+                Oferta de{" "}
+                <span className="font-medium text-blue-500">trabajo</span>
               </p>
             </div>
           </div>
@@ -89,7 +101,10 @@ export default function JobDetailView() {
           <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
             <img
               className="w-full h-full max-w-md"
-              src={data?.icon?.fields?.asset?.fields?.file?.url ?? "https://merakiui.com/images/components/Email-campaign-bro.svg"}
+              src={
+                data?.icon?.fields?.asset?.fields?.file?.url ??
+                "https://merakiui.com/images/components/Email-campaign-bro.svg"
+              }
               alt="email illustration vector art"
             />
           </div>
@@ -117,23 +132,26 @@ export default function JobDetailView() {
 
         <div>
           {data?.applicants ? (
-
-          <button
-            onClick={handleModalOpen}
-            className="w-full text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-blue-700 rounded-md hover:bg-blue-600"
-          >
-            Aplicar ahora
-          </button>
-          ):(
             <button
-            onClick={handleModalOpen}
-            className="w-full text-sm font-medium tracking-wider text-gray-100 uppercase opacity-60 transition-colors duration-300 transform bg-blue-700 rounded-md hover:bg-blue-600"
-          >
-            Aplicar ahora
-          </button>
+              // onClick={handleModalOpen}
+              className="w-full text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-blue-700 rounded-md hover:bg-blue-600 py-4"
+            >
+              Aplicar ahora
+            </button>
+          ) : (
+            <button
+              onClick={handleModalOpen}
+              className="w-full text-sm font-medium tracking-wider text-gray-100 uppercase opacity-60 transition-colors duration-300 transform bg-blue-700 rounded-md hover:bg-blue-600 py-4"
+            >
+              Aplicar ahora
+            </button>
           )}
 
-          <ModalForm isOpen={isModalOpen} onClose={handleModalClose} onSubmit={handleFormSubmit} />
+          <ModalForm
+            isOpen={isModalOpen}
+            onClose={handleModalClose}
+            onSubmit={handleFormSubmit}
+          />
         </div>
       </div>
     </>
