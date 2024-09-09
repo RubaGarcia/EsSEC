0234d1f411fe9c27a429a0be0ad9d886c472ac04
Eduardo Llamosas <eduardo.llamosas@incentro.com>
2024-09-09 15:43:23 +0200

    Changes to auditories

--- frontend/src/views/Services/AuditoryServiceView.tsx
+++ frontend/src/views/Services/AuditoryServiceView.tsx
@@ -2,7 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import { getAuditories } from "../../api/ServicesAPI";
 import HeroAuditories from "../../components/Services/auditories/HeroAuditories";
 import TestimonialAuditories from "../../components/Services/auditories/TestimonialAuditories";
-import type { ApiRequest, Cartridge, Entry, ValuePropositionFields, PersonFields} from "../../types";
+import type { ApiRequest, Cartridge, Entry, ValuePropositionFields, PersonFields, Blurb} from "../../types";
+import { renderRichText } from "../../helpers/RichTextProcessor";
 
 export default function AuditoryServiceView() {
   const { data, isError, isLoading } :  {data: undefined | ApiRequest, isError:boolean, isLoading: boolean} = useQuery({
@@ -13,14 +14,15 @@ export default function AuditoryServiceView() {
 
   if (isLoading || isError) return <p className="bg-white">Loading...</p>;
 
-  const cartridgeHero : Entry<Cartridge>= data?.fields?.sections![0] as  Entry<Cartridge>;
+  const cartridgeHero : Entry<Cartridge>= data?.fields?.sections?.[0] as  Entry<Cartridge>;
+  const auditoriesBlurb : Entry<Blurb>= data?.fields?.sections?.[2] as  Entry<Blurb>;
 
-  const hero: Entry<ValuePropositionFields> = cartridgeHero.fields?.items![0] as Entry<ValuePropositionFields>;
+  const hero: Entry<ValuePropositionFields> = cartridgeHero.fields?.items?.[0] as Entry<ValuePropositionFields>;
 
 
   const reviews:  Entry<PersonFields>[] = []
 
-  const cartridgeServices: Entry<Cartridge>= data?.fields?.sections![1] as Entry<Cartridge>;
+  const cartridgeServices: Entry<Cartridge>= data?.fields?.sections?.[1] as Entry<Cartridge>;
   const productos : Entry<PersonFields>[] = cartridgeServices?.fields?.items as Entry<PersonFields>[];
   
 
@@ -39,10 +41,10 @@ export default function AuditoryServiceView() {
 
       <section className="bg-white dark:bg-gray-900">
         <div className="max-w-6xl px-6 py-10 mx-auto">
-          <p className="text-xl font-medium text-blue-500 ">Testimonials</p>
+          <p className="text-xl font-medium text-blue-500 ">{auditoriesBlurb?.fields?.title}</p>
 
-          <h1 className="mt-2 text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
-            What the clients are saying...
+          <h1 className="mt-4 text-gray-500 xl:mt-6 dark:text-gray-300">
+          { auditoriesBlurb?.fields?.textBlurb && <div dangerouslySetInnerHTML={{ __html: renderRichText(auditoriesBlurb?.fields?.textBlurb ) }} />}
           </h1>
 
           <TestimonialAuditories reviews={reviews}/>
